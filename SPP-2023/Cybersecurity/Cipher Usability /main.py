b012751bd5a906ffa6451a202d3b835587a9a346
CaptainSapphire <122335357+CaptainSapphire@users.noreply.github.com>
2023-12-26 17:22:29 -0800

    Update main.py

--- SPP-2023/Cybersecurity/Cipher Usability /main.py
+++ SPP-2023/Cybersecurity/Cipher Usability /main.py
@@ -1,4 +1,3 @@
-#This program is the starter code for the Cipher Usability Project. 
 # This code is inspired by Cracking Codes with Python: An Introduction to Building and Breaking Ciphers by Al Sweigart https://www.nostarch.com/crackingcodes (BSD Licensed)
 #IMPORTING LIBRARIESSSSS
 import string
